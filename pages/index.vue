ea40ca7fd04656dd6b3c7ea5731cc90c9086bc0d
Falestio <falestioltmpt@gmail.com>
2022-03-30 19:57:59 +0700

    initial

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Tutorial />
+    <div class="first">
+      <div class="second">hi</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="sass" scoped>
+    .second {
+      color: green;
+    }
+</style>
